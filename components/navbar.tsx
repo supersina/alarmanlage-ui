41ca494951c07f3effba7c90e8b53968804c38ec
supersina <sinschkoli@gmail.com>
2022-02-24 12:43:50 +0100

    resolves #24, burger menu

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,68 +1,110 @@
 import { Box, Button, Flex } from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 import Link from "next/link";
 import Image from "next/image";
 import { useSession, signIn, signOut } from "next-auth/react";
+import { useState } from "react";
+import { colors } from "../theme/colors";
 
 export const Navbar = () => {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
-      <Flex
-        as="nav"
-        justify="space-between"
-        wrap="wrap"
-        width="100%"
-        borderBottom="1px solid #eaeaea"
-        p={{ base: 2, sm: 2 }}
-        pr={{ base: 2, sm: 8 }}
-      >
-        <Box width="40px" height="40px">
-          <Link href="/">
-            <a>
-              <Image src="/favicon.ico" width={50} height={50} alt=""></Image>
-            </a>
-          </Link>
-        </Box>
-
-        {session ? (
-          <Flex
-            width={{ base: "80%", xs: "90%", sm: "80%", md: "50%", lg: "40%" }}
-            direction="row"
-            alignItems="flex-end"
-            justifyContent="space-between"
-          >
+      <HamburgerIcon
+        w={6}
+        h={6}
+        display={{ base: isOpen ? "none" : "block", sm: "none" }}
+        style={{ position: "absolute", right: "1rem", top: "0.5rem" }}
+        onClick={() => setIsOpen(!isOpen)}
+      ></HamburgerIcon>
+      <Box display={{ base: isOpen ? "block" : "none", sm: "block" }}>
+        <Flex
+          as="nav"
+          justify="space-between"
+          wrap="wrap"
+          width="100%"
+          borderBottom="1px solid #eaeaea"
+          p={{ base: 2, sm: 2 }}
+          pr={{ base: 2, sm: 8 }}
+          backgroundColor="black"
+        >
+          <Box width="40px" height="40px">
             <Link href="/">
-              <a>Start</a>
+              <a>
+                <Image
+                  src="/favicon.ico"
+                  width={50}
+                  height={50}
+                  alt="Logo"
+                ></Image>
+              </a>
             </Link>
-            <Link href="/alarms">
-              <a>Alarme</a>
-            </Link>
-            <Link href="/alarmsystems">
-              <a>Alarmsysteme</a>
-            </Link>
-            <Link href="/settings">
-              <a>Einstellungen</a>
-            </Link>
+          </Box>
 
-            <Button
-              width="fit-content"
-              colorScheme="yellow"
-              onClick={() => signOut()}
+          {session ? (
+            <Flex
+              width={{
+                base: "100%",
+                xs: "100%",
+                sm: "80%",
+                md: "50%",
+                lg: "40%",
+              }}
+              height={{ base: "12rem", sm: "auto" }}
+              direction={{ base: "column", sm: "row" }}
+              alignItems={{ base: "center", sm: "flex-end" }}
+              alignSelf="flex-end"
+              style={{ order: 2 }}
+              justifyContent="space-between"
+              marginTop={isOpen ? "1rem" : "none"}
             >
-              Sign out
-            </Button>
-          </Flex>
-        ) : (
-          <Button
-            width="fit-content"
-            colorScheme="yellow"
-            onClick={() => signIn()}
+              <Link href="/">
+                <a>Start</a>
+              </Link>
+              <Link href="/alarms">
+                <a>Alarme</a>
+              </Link>
+              <Link href="/alarmsystems">
+                <a>Alarmsysteme</a>
+              </Link>
+              <Link href="/settings">
+                <a>Einstellungen</a>
+              </Link>
+
+              <Box
+                width="fit-content"
+                color={colors.headingColor}
+                onClick={() => signOut()}
+              >
+                Sign out
+              </Box>
+            </Flex>
+          ) : (
+            <Box
+              width="fit-content"
+              color={colors.headingColor}
+              onClick={() => signIn()}
+            >
+              Sign In
+            </Box>
+          )}
+          <Flex
+            width="40px"
+            height="40px"
+            justifyContent="center"
+            alignItems="center"
           >
-            Sign In
-          </Button>
-        )}
-      </Flex>
+            <CloseIcon
+              width={4}
+              height={4}
+              display={{ base: isOpen ? "block" : "none", sm: "none" }}
+              onClick={() => setIsOpen(!isOpen)}
+            ></CloseIcon>
+          </Flex>
+        </Flex>
+      </Box>
     </>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 
 import Head from "next/head";
 import { Flex, Heading, Text } from "@chakra-ui/react";
